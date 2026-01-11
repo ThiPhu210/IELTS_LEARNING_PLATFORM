b9f9de4d98981e1648e4c104df072e0711ab7a1d
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-11 20:34:17 +0700

    Update course_section.rb

--- app/models/course_section.rb
+++ app/models/course_section.rb
@@ -6,6 +6,20 @@ class CourseSection < ApplicationRecord
 
   validates :title, presence: true
   validate :files_size_limit
+  validate :files_type
+
+def files_type
+  files.each do |file|
+    unless file.content_type.in?(%w[
+      application/pdf
+      image/png
+      image/jpg
+      image/jpeg
+    ])
+      errors.add(:files, "Chỉ cho phép PDF hoặc hình ảnh")
+    end
+  end
+end
 
   def files_size_limit
     files.each do |file|
