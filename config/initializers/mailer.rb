684664ae18dd2adcc5d0910fccbabbc70c9510ac
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-13 02:55:47 +0700

    Update mailer.rb

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Rails.application.config.action_mailer.default_url_options = {
-    host: ENV.fetch("ielts-learning-platform.duckdns.org"),
+    host: "ielts-learning-platform.duckdns.org",
     protocol: "https"
   }
 end
