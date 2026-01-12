93dcbc382c35daf07d8fdebb32fe745bbd2d8572
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-13 03:08:28 +0700

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@ielts-platform.test"
 
-  default_url_options[:host] = "host.docker.internal"
-  default_url_options[:port] = 3000
-
   layout "mailer"
 end
