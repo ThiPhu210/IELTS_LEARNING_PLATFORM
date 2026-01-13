052c4e84cc9f830d2a97ec41f1ddcadbd7d8357b
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-13 15:04:16 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # ActionMailer
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("APP_HOST", "ielts-learning-platform.duckdns.org"),
+    host: "https://d34ute7tylgmox.cloudfront.net",
     protocol: "https"
   }
   config.action_mailer.delivery_method = :smtp
