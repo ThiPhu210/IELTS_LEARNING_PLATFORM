06a1bea101ee76575e08022efd0140670161a33d
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-13 15:03:10 +0700

    Update default_url_options.rb

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,4 +1,4 @@
-host = "ielts-learning-platform.duckdns.org"
+host = "https://d34ute7tylgmox.cloudfront.net"
 
 Rails.application.routes.default_url_options[:host] = host
 Rails.application.routes.default_url_options[:protocol] = "https"
