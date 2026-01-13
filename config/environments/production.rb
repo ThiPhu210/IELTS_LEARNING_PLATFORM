43c81a83bb6bb5de55f1ef653c8e948329cdbe7a
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-14 01:53:58 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,11 @@ Rails.application.configure do
   config.active_storage.service = :amazon
   config.assume_ssl = true
   config.force_ssl = true
+  config.session_store :cookie_store,
+    key: "_ielts_session",
+    secure: true,       
+    same_site: :lax      
+
 
   config.log_tags = [:request_id]
   config.logger = ActiveSupport::TaggedLogging.logger($stdout)
