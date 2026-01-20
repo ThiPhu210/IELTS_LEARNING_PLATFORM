5381e104b5a3f5ac5e408f12684f3246a06a86fe
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-21 05:08:23 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,29 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
-        :registerable,
-        :recoverable,
-        :rememberable,
-        :validatable,
-        :confirmable
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+  has_many :orders, dependent: :destroy
+  has_many :courses, through: :orders
 
   has_many :course_accesses
-  has_many :courses, through: :orders
-  has_many :orders, dependent: :destroy
   has_many :speaking_attempts
   has_many :course_progresses
-  has_one :teacher_profile, dependent: :destroy
 
+  has_one :teacher_profile, dependent: :destroy
   has_one_attached :thumbnail
   accepts_nested_attributes_for :teacher_profile, allow_destroy: true
 
   enum :role, { admin: 0, teacher: 1, student: 2 }, suffix: true
   scope :teachers, -> { where(role: :teacher) }
+
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
   validates :full_name, presence: true
   validates :password, presence: true, on: :create
+
   def has_course_access?(course)
     course_accesses.exists?(
       course_id: course.id,
