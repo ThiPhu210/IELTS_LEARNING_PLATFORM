09b7745aa306122fefed68f189858b49c06f866a
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-21 00:01:27 +0700

    Update vnpay.rb

--- config/initializers/vnpay.rb
+++ config/initializers/vnpay.rb
@@ -2,5 +2,4 @@ VN_PAY = {
   tmn_code: "9APTANC1",
   hash_secret: "OV71K9S7ITDX3J2HF113O886GMZR72ZP",
   url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-  return_url: "https://d34ute7tylgmox.cloudfront.net/students/vnpay/return"
 }
