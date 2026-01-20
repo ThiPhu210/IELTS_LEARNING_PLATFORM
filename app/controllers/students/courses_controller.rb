332de264ab31d1c45406a7eb227c27f2e8c12d04
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-20 22:33:47 +0700

    Update courses_controller.rb

--- app/controllers/students/courses_controller.rb
+++ app/controllers/students/courses_controller.rb
@@ -1,29 +1,70 @@
-class Students::CoursesController < ApplicationController
-  layout "students"
+class Admin::CoursesController < Admin::BaseController
   before_action :authenticate_user!
-  before_action :require_course_access!, only: [ :show ]
+  before_action :require_admin
   def index
-    if params[:paid] == "true"
-      @courses = Course.joins(:orders)
-                       .where(orders: {
-                         user_id: current_user.id,
-                         status: :paid
-                       })
-                       .distinct
-    else
-      @courses = Course.all
-    end
+  @courses = Course
+              .order(created_at: :desc)
+              .page(params[:page])
+              .per(4)
   end
-  def show
+
+
+  def edit
     @course = Course.find(params[:id])
-  end
-  private
 
-  def require_course_access!
-    course = Course.find(params[:id])
-
-    unless current_user&.has_course_access?(course)
-      redirect_to students_courses_path, alert: "Bạn cần thanh toán để truy cập khóa học"
+    if @course.course_sections.empty?
+      section = @course.course_sections.build
+      section.lessons.build
+    else
+      @course.course_sections.each do |section|
+        section.lessons.build if section.lessons.empty?
+      end
     end
   end
+
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      flash[:success] = "✅ Bạn vừa thay đổi thành công thông tin khóa học"
+      redirect_to admin_courses_path, notice: "Cập nhật khóa học thành công"
+
+    else
+      Rails.logger.debug @course.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
+    Rails.logger.debug params[:course]
+  end
+
+  def destroy
+    course = Course.find(params[:id])
+    course.destroy
+    redirect_to admin_courses_path, notice: "Đã xóa khóa học"
+  end
+
+  private
+  def course_params
+  params.require(:course).permit(
+    :title,
+    :description,
+    :price,
+    :thumbnail,
+    course_sections_attributes: [
+      :id,
+      :title,
+      :_destroy,
+      lessons_attributes: [
+        :id,
+        :title,
+        :duration,
+        :video,
+        :_destroy,
+        { pdfs: [] }
+      ]
+    ]
+  )
 end
+
+  end
+  
