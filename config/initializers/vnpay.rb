9f314e5eb0681ecb620770b93ea519509abb3b41
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-20 22:45:36 +0700

    Update vnpay.rb

--- config/initializers/vnpay.rb
+++ config/initializers/vnpay.rb
@@ -1,6 +1,6 @@
 VN_PAY = {
-  tmn_code: ENV["VNP_TMN_CODE"],
-  hash_secret: ENV["VNP_HASH_SECRET"],
+  tmn_code: "9APTANC1",
+  hash_secret: "OV71K9S7ITDX3J2HF113O886GMZR72ZP",
   url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   return_url: "https://resigned-unincreased-agnus.ngrok-free.dev/students/vnpay/return"
 }
