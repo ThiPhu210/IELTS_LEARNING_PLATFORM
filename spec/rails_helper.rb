97dcde52e5fb4651da0662b1fe4960f1abc094cf
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-26 05:09:29 +0700

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+  config.filter_run_excluding type: :system if ENV["CI"]
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include FactoryBot::Syntax::Methods
