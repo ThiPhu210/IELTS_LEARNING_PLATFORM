1640b941c0c62e14634165384827d5b08004feca
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-25 20:11:36 +0700

    Update payment.rb

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :order
-  has_one :course_access
+  has_one :course_access, dependent: :destroy
 
   enum :status, { pending: 0, paid: 1, failed: 2 }, suffix: true
   scope :paid_payments, -> { where(status: statuses[:paid]) }
