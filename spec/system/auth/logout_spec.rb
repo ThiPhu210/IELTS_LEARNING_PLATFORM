c9e6cfa879f79a36630104a55b7e661ff59646e8
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-26 08:56:45 +0700

    Update logout_spec.rb

--- spec/system/auth/logout_spec.rb
+++ spec/system/auth/logout_spec.rb
@@ -1,27 +1,27 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe "Logout", type: :system do
-#   let(:student) { create(:user, confirmed_at: Time.current, password: "password123") }
+RSpec.describe "Logout", type: :system do
+  let(:student) { create(:user, confirmed_at: Time.current, password: "password123") }
 
-#   before do
-#     driven_by(:selenium_chrome_headless)
-#   end
+  before do
+    driven_by(:selenium_chrome_headless)
+  end
 
-#   def login
-#     visit new_user_session_path
-#     fill_in "Email", with: student.email
-#     fill_in "Password", with: "password123"
-#     click_button "Login to your account"
-#     expect(page).to have_current_path(students_dashboard_path)
-#   end
+  def login
+    visit new_user_session_path
+    fill_in "Email", with: student.email
+    fill_in "Password", with: "password123"
+    click_button "Login to your account"
+    expect(page).to have_current_path(students_dashboard_path)
+  end
 
-#   it "logs out successfully" do
-#     login
+  it "logs out successfully" do
+    login
 
-#     page.execute_script <<~JS
-#   document.querySelector("form input[name='_method'][value='delete']").closest("form").submit();
-#     JS
+    page.execute_script <<~JS
+  document.querySelector("form input[name='_method'][value='delete']").closest("form").submit();
+    JS
 
-#     expect(page).to have_current_path(new_user_session_path)
-#   end
-# end
+    expect(page).to have_current_path(new_user_session_path)
+  end
+end
