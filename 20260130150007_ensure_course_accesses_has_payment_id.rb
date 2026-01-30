c6c75c64ae2d0991fb0f866280e2b3a34b9a00cd
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-30 12:43:38 +0700

    Delete 20260130150007_ensure_course_accesses_has_payment_id.rb

--- 20260130150007_ensure_course_accesses_has_payment_id.rb
+++
@@ -1,6 +0,0 @@
-class EnsurePaymentOnCourseAccesses < ActiveRecord::Migration[7.1]
-
-def change
-  add_reference :course_accesses, :payment, foreign_key: true unless column_exists?(:course_accesses, :payment_id)
-end
-end
