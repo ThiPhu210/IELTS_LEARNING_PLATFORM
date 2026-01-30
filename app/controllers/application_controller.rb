1709ce48da13fcf590f5066ea781a68b7d28ea3c
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-30 15:41:34 +0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name ])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :role, :school])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :role, :school])
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :remember_me ])
   end
 
