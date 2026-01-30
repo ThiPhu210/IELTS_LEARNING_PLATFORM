21ba17e7e2de69e3cb030fb5ee62300ef43b761f
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-01-30 12:46:59 +0700

    Update 20251229011701_create_payments.rb

--- db/migrate/20251229011701_create_payments.rb
+++ db/migrate/20251229011701_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration[8.0]
   def change
     create_table :payments do |t|
       t.references :course_access, null: false, foreign_key: true
-      t.references :order, foreign_key: true
+      t.references :order, null: false, foreign_key: true
 
       t.decimal :amount
       t.string :payment_method
