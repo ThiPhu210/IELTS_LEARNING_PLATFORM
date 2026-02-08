d1c1c78f161dcf67dce66aeea61f3e1b3c4b2846
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-08 22:09:44 +0700

    Update courses_controller.rb

--- app/controllers/students/courses_controller.rb
+++ app/controllers/students/courses_controller.rb
@@ -1,29 +1,37 @@
 class Students::CoursesController < ApplicationController
   layout "students"
   before_action :authenticate_user!
-  before_action :require_course_access!, only: [ :show ]
-def index
-  if params[:paid] == "true"
-    @courses = current_user
-      .course_accesses
-      .active_status
-      .includes(:course)
-      .map(&:course)
-  else
-    @courses = Course.all
+  before_action :require_course_access!, only: [:show]
+
+  def index
+    courses =
+      if params[:paid] == "true"
+        Course
+          .joins(:course_accesses)
+          .merge(current_user.course_accesses.active_status)
+          .distinct
+      else
+        Course.all
+      end
+
+    @courses = courses
+      .order(created_at: :desc)
+      .page(params[:page])
+      .per(9)
   end
-end
 
   def show
     @course = Course.find(params[:id])
   end
+
   private
 
   def require_course_access!
     course = Course.find(params[:id])
 
     unless current_user&.has_course_access?(course)
-      redirect_to students_courses_path, alert: "Bạn cần thanh toán để truy cập khóa học"
+      redirect_to students_courses_path,
+                  alert: "Bạn cần thanh toán để truy cập khóa học"
     end
   end
 end
