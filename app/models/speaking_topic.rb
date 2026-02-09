851f11c2cd4ff6c4e8199a689b343a5f634b602e
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-09 15:47:34 +0700

    Update speaking_topic.rb

--- app/models/speaking_topic.rb
+++ app/models/speaking_topic.rb
@@ -1,8 +1,8 @@
 class SpeakingTopic < ApplicationRecord
   belongs_to :lesson
-  has_many :speaking_questions, dependent: :destroy
   has_many :speaking_attempts
-
+  has_many :speaking_questions, dependent: :destroy
+  accepts_nested_attributes_for :speaking_questions, allow_destroy: true
   # enum part: {
   #   part1: "part1",
   #   part2: "part2",
