4458b89478ba863b1184f7c342f7d400347fa83e
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-09 20:10:39 +0700

    Update speaking_question.rb

--- app/models/speaking_question.rb
+++ app/models/speaking_question.rb
@@ -3,10 +3,7 @@ class SpeakingQuestion < ApplicationRecord
 
   validates :preparation_time, numericality: { greater_than: 0 }
   validates :speaking_time, numericality: { greater_than: 0 }
-
   validates :question_text, length: { maximum: 1000 }
   validates :cue_card, length: { maximum: 1000 }
-  validates :transcript, length: { maximum: 1000 }
-  validates :feedback, length: { maximum: 1000 }
-  def transcript=(val); end
+
 end
