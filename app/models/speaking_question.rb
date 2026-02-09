973cf8ad90417930cf8ad6bab0f29b07c046ece6
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-09 16:33:42 +0700

    Update speaking_question.rb

--- app/models/speaking_question.rb
+++ app/models/speaking_question.rb
@@ -1,9 +1,5 @@
 class SpeakingQuestion < ApplicationRecord
   belongs_to :speaking_topic
-  validates :question_text, presence: true
-  validates :cue_card, presence: true
-  validates :preparation_time, presence: true
-  validates :speaking_time, presence: true
 
   validates :preparation_time, numericality: { greater_than: 0 }
   validates :speaking_time, numericality: { greater_than: 0 }
