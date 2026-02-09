11f8bd2c61a441e858542e77d4a5a596a4909b44
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-09 20:23:22 +0700

    Update speaking_question.rb

--- app/models/speaking_question.rb
+++ app/models/speaking_question.rb
@@ -1,9 +1,19 @@
 class SpeakingQuestion < ApplicationRecord
   belongs_to :speaking_topic
 
-  validates :preparation_time, numericality: { greater_than: 0 }
-  validates :speaking_time, numericality: { greater_than: 0 }
-  validates :question_text, length: { maximum: 1000 }
-  validates :cue_card, length: { maximum: 1000 }
+  validates :preparation_time,
+            numericality: { greater_than: 0 },
+            allow_blank: true
 
+  validates :speaking_time,
+            numericality: { greater_than: 0 },
+            allow_blank: true
+
+  validates :question_text,
+            length: { maximum: 1000 },
+            allow_blank: true
+
+  validates :cue_card,
+            length: { maximum: 1000 },
+            allow_blank: true
 end
