761540c89268856252ef5d6065bb7c65a64745a4
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-02-10 11:31:22 +0700

    Update speaking_attempts_spec.rb

--- spec/requests/students/speaking_attempts_spec.rb
+++ spec/requests/students/speaking_attempts_spec.rb
@@ -1,11 +1,31 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Students::SpeakingAttempts", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/students/speaking_attempts/create"
-      expect(response).to have_http_status(:success)
-    end
+
+  let(:user) { create(:user) }
+  let(:course) { create(:course) }
+  let(:topic) { create(:speaking_topic) }
+
+  before do
+    sign_in user
   end
 
+  it "creates speaking attempt" do
+
+    file = fixture_file_upload(
+      Rails.root.join("spec/fixtures/audio.webm"),
+      "audio/webm"
+    )
+
+    post "/students/speaking_attempts",
+         params: {
+           audio: file,
+           speaking_topic_id: topic.id,
+           course_id: course.id,
+           part: "part2"
+         }
+
+    expect(response).to have_http_status(:ok)
+
+  end
 end
