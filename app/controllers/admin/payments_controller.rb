3fef69648365cca94689413bb227013cf0f1aa84
Nguyễn Thi Phú <133946664+ThiPhu210@users.noreply.github.com>
2026-03-02 09:13:47 +0700

    Update payments_controller.rb

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,7 +1,4 @@
-class Admin::PaymentsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_admin
-
+class Admin::PaymentsController < Admin::BaseController
   def index
     @payments = Payment.all.includes(:order, :course_access).order(created_at: :desc)
   end
